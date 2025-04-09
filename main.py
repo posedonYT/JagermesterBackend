92d11bcded14c54f10704a978e7784f5dd674a78
posedonYT <vvdard@gmail.com>
2025-04-09 15:48:02 +0700

    main update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import multiprocessing
 
 
 def run_api():
-    uvicorn.run(app, host="https://jagermesterbackend.onrender.com", port=4000)
+    uvicorn.run(app, host="44.226.145.213", port=4000)
 
 
 async def run_bot():
